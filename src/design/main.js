f5979371ac41d7f9387cab3e05f9524f13ef3e89
Segr <segr@nashalife.ru>
2017-02-15 12:43:49 +0300

    Исправлена шапка Добавлен подсчет перемешений в метрике

--- src/design/main.js
+++ src/design/main.js
@@ -10,22 +10,25 @@ $q(document).ready(function($){
 		(screen.width-$width)/2,
 		(screen.height-$height)/2
 		);
- 	$q("div#pages").tabs("div#pages div.page", {
+ 	$("div#pages").tabs("div#pages div.page", {
 		tabs: 'h2.title'
 	});
- 	$q("div.page").tabs("div.page div.menu", {
+ 	$("div.page").tabs("div.page div.menu", {
 		tabs:  'h3.title'
 	});
-	$q("#pages .title").hover( function() {
+	$("#pages .title").hover( function() {
 		$(this).addClass("hover");
 	}, function() {
 		$(this).removeClass("hover");
 	});
-	$q("#pages .title").click( function() {
-		window.location.hash = $q(this).parent().attr('id');
+	$("#pages .title").click( function() {
+		id = $(this).parent().attr('id');
+		if (yaCounter42800219) yaCounter42800219.hit('file://autorun.hta#'+id, {title: $(this).text(), referer: 'file://autorun.hta#'+window.location.hash});
+		window.location.hash = id;
 	});
-	$q("a.runme").click(function() {
+	$("a.runme").click(function() {
 		$file = $(this).attr("href");
+		if (yaCounter42800219) yaCounter42800219.extLink($file);
 		$file = $file.split('/').join('\\');
 		var wss = new ActiveXObject("WScript.Shell");
 		if (wss) {
